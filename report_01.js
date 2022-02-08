9d765f452d61f489da5795cec314ac6820313652
long <bekeo0302@gmail.com>
2022-02-08 16:44:24 +0700

    commit report

--- report_01.js
+++ report_01.js
@@ -2,16 +2,17 @@
 // 0.1 + 0.2 != 0.3
 
 function myFuc() {
-    let a = 0.1
-    let b = 0.2
-    let c = 0.3
+    const a = 0.1
+    const b = 0.2
+    const c = 0.3
 
-    let d = a + b
+    const d = a + b
 
-    let e = +d.toFixed(2)
+    const e = +d.toFixed(2)
 
     return e === c
 }
+
 console.log("fix floating point", myFuc());
 
 // so sánh 2 obj
@@ -22,7 +23,8 @@ function isObject(obj) {
 
 // Hàm so sánh sâu
 function isDeepEqual(obj1, obj2) {
-    const keys1 = Object.keys(obj1); // trả về mảng các thuộc tính của obj1
+    const keys1 = Object.keys(obj1);
+    console.log("keys1", keys1); // trả về mảng các thuộc tính của obj1
     const keys2 = Object.keys(obj2); // trả về mảng các thuộc tính của obj2
 
     // nếu số lượng keys khác nhau thì chắc chắn khác nhau
@@ -51,8 +53,8 @@ function isDeepEqual(obj1, obj2) {
     return true;
 }
 
-let point1 = { x: 1, y: 2, metadata: { type: "point" } };
-let point2 = { x: 1, y: 2, metadata: { type: "point" } };
+let point1 = { x: 1, y: 2, metadata: { type: "point", age: 12 } };
+let point2 = { x: 1, y: 2, meta: { type: "point" } };
 
 console.log("so sánh 2 object ", isDeepEqual(point1, point2));
 
@@ -98,12 +100,81 @@ var number = 1
 console.log(number++, number);
 
 //scope
-//var :có thể khai báo lại
-var i = 1;
-var i = 2;
-var obj = {
-    name: "string"
-}
-console.log(typeof JSON.stringify(obj))
 
-//let : chỉ có thể cập nhật lại giá trị và phạm vi là local
+
+//let : chỉ có thể cập nhật lại giá trị và phạm vi là  block scoped k thể tái khái bao trong cùng 1 block  nhưng có thể nếu không chung scope được khai báo  
+// let không gán giá trị sẽ là undefined
+let a = 10;
+a = 11;
+console.log(a);
+a = 11
+    // ra ngoài block
+if (true) {
+    let a = 12
+    console.log("block riêng", a); //vẫn được 
+}
+// let a = 12; //error
+//const có 2 loại 
+// Trong biến const nếu trường hợp kiểu của biến là primitive (bao gồm string, number, boolean, null, và undefined) thì chúng ta sẽ không thể tái khai báo hay cập nhật giá trị mới để thay thế cho giá trị trước đó của biến
+const b = 10;
+// b = 11 //error
+
+// Đối với trường hợp kiểu biến là reference (bao gồm object, array, và function) thì tuy không thể tái khai báo hay cập nhật giá trị của biến nhưng chúng ta vẫn có thể cập nhật giá trị cho thuộc tính của biến đó.
+const arr = []
+arr.push(1)
+console.log("arr", arr);
+
+// var dùng cho toàn cùng có thể tái khái bao phạm vị toàn cùng nhưng nhược điểm là  code của chúng ta lên đến hàng trăm, hàng nghìn dòng code, hoặc chúng ta cũng không biết được giá trị của biến liệu có bị thay đổi ở đoạn code nào 
+if (true) {
+    var greeting = "name"
+    console.log(greeting);
+}
+console.log(greeting);
+
+var greeting = "hey hi";
+
+var times = 4;
+
+if (times > 3) {
+    var greeting = "say Hello instead";
+}
+
+console.log(greeting); //"say Hello instead"
+
+//hoisting
+console.log(greeting);
+var greeting = "say hello";
+//sẽ được biên dịch là:
+var greeting;
+console.log(greeting); // greeting is undefined
+greeting = "say hello";
+// do  biến var còn có thêm tính chất hoisting: nghĩa là dù khai báo ở đâu thì biến đều sẽ được đem lên đầu scope trước khi code được thực hiện. nên t có thêm use stric để đảm bảo vấn đề này
+//"use strict";Xác định rằng mã JavaScript phải được thực thi ở "chế độ nghiêm ngặt" nhằm tránh hoisting.
+
+//Function scope// Scope A
+// var myFunction = function() {
+// Scope B
+//     var myOtherFunction = function() {
+// Scope C
+//     };
+// };
+//Lexical Scope
+
+// Scope A
+// var myFunction = function() {
+// Scope B
+//     var name = 'Bap'; // định nghĩa trong Scope B
+//     var myOtherFunction = function() {
+// Scope C: `name` có thể được truy cập tại đây
+//     };
+// };
+
+//Closures là returun của 1 function trả về function
+// function(name) {
+//     var text = 'Hello, ' + name; // scope cha
+//  đây là Closures:
+//return function() {
+//
+//         console.log(text); // có thể truy cập từ scope con.
+//     };
+// };
